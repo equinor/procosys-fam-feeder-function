e10a722dffdf325756592f50d7003f19899b9d71
Tord Joranger <TORDJ@equinor.com>
2022-02-23 13:37:30 +0100

    propper commit

---
+++ Infrastructure/Repositories/BlobRepository.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Infrastructure.Repositories;
+
+public class BlobRepository
+{
+    private readonly BlobContainerClient _client;
+
+    public BlobRepository(string connectionString, string containerName)
+    {
+        _client = new BlobContainerClient(connectionString, containerName);
+    }
+
+    public async void Download(string pathAndFileName, string downloadPath)
+    {
+        var blobClient = _client.GetBlobClient(pathAndFileName);
+        if (await blobClient.ExistsAsync())
+        {
+            await blobClient.DownloadToAsync(downloadPath);
+        }
+    }
+}
