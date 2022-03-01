a4c48e87ba8738ea8917baf33a23625c425edade
Tord Joranger <TORDJ@equinor.com>
2022-03-01 17:40:23 +0100

    minor

--- Infrastructure/ServiceCollectionSetup.cs
+++ Infrastructure/ServiceCollectionSetup.cs
@@ -8,12 +8,6 @@ namespace Infrastructure;
 
 public static class ServiceCollectionSetup
 {
-    /**
-     * Maximum open cursors in the Pcs database is configured to 300 as per 05.03.2020.
-     * When doing batch updates/inserts, oracle opens a cursor per update/insert to keep track of
-     * the amount of entities updated. The default seems to be 200, but we're setting it explicitly anyway
-     * in case the default changes in the future. This is to avoid ORA-01000: maximum open cursors exceeded.
-     */
     private const int MaxOpenCursors = 200;
 
     public static readonly LoggerFactory LoggerFactory =
