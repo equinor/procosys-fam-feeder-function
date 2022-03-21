13991dc8b125d2bb6d82489dfbc1a0526e7a4435
Tord Joranger <TORDJ@equinor.com>
2022-03-21 13:36:11 +0100

    query error

--- Infrastructure/Repositories/Queries/StockQuery.cs
+++ Infrastructure/Repositories/Queries/StockQuery.cs
@@ -6,8 +6,8 @@ public class StockQuery
     {
         return @$"select
             '{{""Plant"" : ""' || s.projectschema || 
-            '"", ""StockId"" : ' || s.id || 
-            '"", ""StockNo"" : ' ||regexp_replace(s.stockno, '([""\])', '\\\1')  || 
+            '"", ""StockId"" : ""' || s.id || 
+            '"", ""StockNo"" : ""' ||regexp_replace(s.stockno, '([""\])', '\\\1')  || 
             '"", ""Description"" : ""' || regexp_replace(s.description, '([""\])', '\\\1') || 
             '"", ""LastUpdated"" : ""' || TO_CHAR(s.LAST_UPDATED, 'YYYY-MM-DD hh:mm:ss')  ||
             '""}}'  as message
