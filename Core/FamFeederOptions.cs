abc108c51beef25fb976905199332cd25726c340
Terje Woldseth <TWOLD@equinor.com>
2022-07-13 16:06:49 +0200

    configurable btach-size and batch-delay

--- Core/FamFeederOptions.cs
+++ Core/FamFeederOptions.cs
@@ -15,4 +15,7 @@ public class FamFeederOptions
     public string? SearchIndexEndpoint { get; set; }
     public string? SearchIndexName { get; set; }
     public string? SearchIndexAccessKey { get; set; }
+    public string? SearchIndexBatchSize { get; set; }
+    public string? SearchIndexBatchDelay { get; set; }
+
 }
