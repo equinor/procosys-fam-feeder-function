7713792bf0b2862751a49a4fd33901d95da68185
Tord Joranger <TORDJ@equinor.com>
2022-11-21 09:24:50 +0100

    add files

---
+++ Core/WoCutoffOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Core;
+public class WoCutoffOptions
+{
+    public WoCutoffOptions()
+    {
+    }
+
+    public WoCutoffOptions(string? enabledPlants)
+    {
+        EnabledPlants = enabledPlants;
+    }
+
+    public string? EnabledPlants { get; set; }
+}
