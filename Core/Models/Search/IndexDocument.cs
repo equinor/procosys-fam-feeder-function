bf3513444e4f5cb08a7c9e75bdcf20b494558d38
terjewoldseth <78787080+terjewoldseth@users.noreply.github.com>
2022-11-25 08:36:55 +0100

    Added ProCoSys guid (#9)

--- Core/Models/Search/IndexDocument.cs
+++ Core/Models/Search/IndexDocument.cs
@@ -7,6 +7,7 @@
         public string Plant { get; set; } = "";
         public string PlantName { get; set; } = "";
         public string Project { get; set; } = "";
+        public string ProCoSysGuid { get; set; } = "";
         public List<string> ProjectNames { get; set; } = new List<string>();
         public CommPkg? CommPkg { get; set; }
         public McPkg? McPkg { get; set; }
