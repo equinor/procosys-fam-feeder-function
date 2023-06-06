916c52875f5057d0152d0d31ba36ab16d1246c27
Tord Joranger <TORDJ@equinor.com>
2023-06-06 10:45:30 +0200

    fix guid handler

--- Infrastructure/Handlers/GuidTypeHandler.cs
+++ Infrastructure/Handlers/GuidTypeHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Text;
 using Dapper;
 
 namespace Infrastructure.Handlers;
@@ -7,11 +8,28 @@ public class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
 {
     public static readonly GuidTypeHandler Default = new();
 
-    public override Guid Parse(object value) =>
-        Guid.TryParse(value.ToString(), out var guid)
+    public override Guid Parse(object value)
+    {
+        var result = Guid.TryParse(value.ToString(), out var guid)
             ? guid
-            : new Guid((byte[])value);
+            : Guid.TryParse(ByteArrayToHexString((byte[])value), out var guid2)
+                ? guid2
+                :throw new Exception("Could not parse Guid");
+        return result;
+    }
 
     public override void SetValue(IDbDataParameter parameter, Guid value)
         => throw new NotImplementedException();
+        
+    private static string ByteArrayToHexString(IReadOnlyCollection<byte> bytes)
+    {
+        var result = new StringBuilder(bytes.Count * 2);
+
+        foreach (var b in bytes)
+        {
+            result.Append(b.ToString("x2"));
+        }
+
+        return result.ToString();
+    }
 }
