08de69548e5bf6e35dc38bb86b29fa4d33ccffda
haavard-hoijord <hhoij@equinor.com>
2024-02-13 13:08:51 +0100

    109723 - Fix null plant and topic

--- FamFeederFunction/Functions/FamFeeder/TopicHttpTrigger.cs
+++ FamFeederFunction/Functions/FamFeeder/TopicHttpTrigger.cs
@@ -55,8 +55,8 @@ public class TopicHttpTrigger
 
     private static async Task<(string? topicsString, string? plants)> DeserializeTopicAndPlant(HttpRequest req)
     {
-        string? topicString = req.Query["PcsTopic"].ToString() ?? req.Query["PcsTopics"];
-        string? plant = req.Query["Plant"].ToString() ?? req.Query["Plants"];
+        string? topicString = req.Query["PcsTopic"];
+        string? plant = req.Query["Plant"];
 
         var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         dynamic? data = JsonConvert.DeserializeObject(requestBody);
