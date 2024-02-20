7410164c2983c4c02c3955ad989d7374ac7ebb08
haavard-hoijord <hhoij@equinor.com>
2024-02-20 10:23:01 +0100

    Fixes multiplant arguments on famfeeder

--- FamFeederFunction/Functions/FamFeeder/TopicOrchestrator.cs
+++ FamFeederFunction/Functions/FamFeeder/TopicOrchestrator.cs
@@ -25,33 +25,29 @@ public static class TopicOrchestrator
             return new List<string> { "Please provide one or more valid plants" };
         }
 
-        if (param.PcsTopics.Contains(PcsTopicConstants.WorkOrderCutoff, StringComparer.InvariantCultureIgnoreCase))
-        {
-            returnValue.AddRange(await RunMultiPlantWoCutoffOrchestration(context, param.Plants));
+        var allPlants = new List<string>();
+        allPlants.AddRange(param.Plants);
 
-            foreach (var plant in param.Plants)
+        foreach (var plant in param.Plants)
+        {
+            if (MultiPlantConstants.TryGetByMultiPlant(plant, out var validMultiPlants))
             {
-                if (MultiPlantConstants.TryGetByMultiPlant(plant, out var validMultiPlants))
-                {
-                    returnValue.AddRange(await RunMultiPlantWoCutoffOrchestration(context, validMultiPlants));
-                }
+                allPlants.Remove(plant);
+                allPlants.AddRange(validMultiPlants.Except(allPlants));
             }
         }
 
-        foreach (var plant in param.Plants)
+        if (param.PcsTopics.Contains(PcsTopicConstants.WorkOrderCutoff, StringComparer.InvariantCultureIgnoreCase))
+        {
+            returnValue.AddRange(await RunMultiPlantWoCutoffOrchestration(context, allPlants));
+        }
+
+        foreach (var plant in allPlants)
         {
             var newParamList = param.PcsTopics
                 .Where(s => s != PcsTopicConstants.WorkOrderCutoff)
                 .Select(s =>
                     new QueryParameters(plant, s)).ToList();
-
-            if (MultiPlantConstants.TryGetByMultiPlant(plant, out var validMultiPlants))
-            {
-                foreach (var newParam in newParamList)
-                {
-                    returnValue.AddRange(await RunMultiPlantOrchestration(context, validMultiPlants, newParam));
-                }
-            }
 
             foreach (var newParam in newParamList)
             {
@@ -68,20 +64,21 @@ public static class TopicOrchestrator
         var param = context.GetInput<QueryParameters>();
         var allPlants = await context.CallActivityAsync<List<string>>(nameof(GetValidPlantsActivity), null);
 
-        return param.Plants.Any(s => allPlants.Contains(s, StringComparer.InvariantCultureIgnoreCase));
+        var newPlants = new List<string>();
+        newPlants.AddRange(param.Plants);
+
+        foreach (var plant in param.Plants)
+        {
+            if (MultiPlantConstants.TryGetByMultiPlant(plant, out var validMultiPlants))
+            {
+                newPlants.Remove(plant);
+                newPlants.AddRange(validMultiPlants.Except(newPlants));
+            }
+        }
+
+        return newPlants.Any(s => allPlants.Contains(s, StringComparer.InvariantCultureIgnoreCase));
     }
 
-    private static async Task<List<string>> RunMultiPlantOrchestration(IDurableOrchestrationContext context, IEnumerable<string> validMultiPlants,
-        QueryParameters param)
-    {
-        var results = validMultiPlants
-            .Select(plant => new QueryParameters(new List<string> {plant}, param.PcsTopics))
-            .Select(input => context.CallActivityAsync<string>(nameof(TopicActivity), input))
-            .ToList();
-        var finishedTasks = await Task.WhenAll(results);
-        return finishedTasks.ToList();
-    }
-    
     private static async Task<List<string>> RunMultiPlantWoCutoffOrchestration(IDurableOrchestrationContext context, IEnumerable<string> validMultiPlants)
     {
         var weekNumbers = new List<string> { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53" };
