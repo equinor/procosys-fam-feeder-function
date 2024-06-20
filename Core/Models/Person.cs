e252a2ea195d714de8c4f047fc71ffd5da722030
haavard-hoijord <hhoij@equinor.com>
2024-06-20 13:48:20 +0200

    Revert "Revert merge dev into master"
    This reverts commit 03cf5976365f6722b15987b59905e8d5be0292e9.

---
+++ Core/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using Equinor.ProCoSys.PcsServiceBus.Interfaces;
+
+namespace Core.Models;
+
+public struct Person : IPersonEventV1
+{
+    public Guid ProCoSysGuid { get; init; }
+    public Guid? AzureOid { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public string UserName { get; init; }
+    public string Email { get; init; }
+    public bool SuperUser { get; init; }
+    public DateTime LastUpdated { get; init; }
+    public string EventType { get; init; }
+}
---
+++ Infrastructure/CompletionQueries/PunchCommentsQuery.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Equinor.ProCoSys.PcsServiceBus.Queries;
+
+namespace Infrastructure.CompletionQueries;
+
+public static class PunchCommentsQuery
+{
+    public static (string query, DynamicParameters parameters) GetQuery(string? plant = null)
+    {
+        QueryHelper.DetectFaultyPlantInput(plant);
+        long? discard = null;
+        var whereClause = QueryHelper.CreateWhereClause(discard, plant, "pc", "irrelevant");
+
+        var query = @$"select
+            pc.projectschema as Plant,
+            pc.procosys_guid as ProCoSysGuid,
+            pli.procosys_guid as PunchItemGuid,
+            pc.text as Text,
+            pc.createdAt as CreatedAt,
+            p.azure_oid as CreatedByGuid,
+            pc.last_updated as LastUpdated
+        from punchlistitemcomment pc
+            join punchlistitem pli on pli.punchlistitem_id = pc.punchlistitem_id
+            join tagcheck tc on tc.tagcheck_id = pli.tagcheck_id
+            join TagFormularType tft ON tc.TagFormularType_Id = tft.TagFormularType_Id
+            join FormularType ft ON tft.FormularType_Id = ft.FormularType_Id
+            join Tag t on tft.Tag_Id = t.tag_id
+            join Project p on p.project_id = t.project_id and p.isvoided = 'N'
+            join projectschema ps on ps.projectschema = pc.projectschema and ps.isvoided = 'N'
+            join person p on p.person_id = pc.createdby_id
+        {whereClause.clause}
+        ";
+
+        return (query, whereClause.parameters);
+        
+    }
+}
